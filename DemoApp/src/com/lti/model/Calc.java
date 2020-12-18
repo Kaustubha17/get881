13f3641ef667b3be83cd53a21ad8f189d845289c
Kaustubha17 <kaustubhashah@gmail.com>
2020-12-18 15:31:35 +0530

    Removed div and prod methods

--- DemoApp/src/com/lti/model/Calc.java
+++ DemoApp/src/com/lti/model/Calc.java
@@ -11,14 +11,7 @@ public void subs(int num1,int num2) {
 public void multi(int num1,int num2) {
 	System.out.print("Multiply="+(num1*num2));
 }
-public void div(int num1,int num2){
-System.out.print("Division="+ (num1/num2));
 
-}
-
-public void prod(int num1,int num2) {
-	System.out.print("Multiply="+(num1*num2));
-}
 	
 
 }
