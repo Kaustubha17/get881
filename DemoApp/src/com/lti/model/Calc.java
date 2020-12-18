9057f9a37b4b9545f8822e3290810315003e1a32
Kaustubha17 <76148907+Kaustubha17@users.noreply.github.com>
2020-12-18 15:42:44 +0530

    Update Calc.java

--- DemoApp/src/com/lti/model/Calc.java
+++ DemoApp/src/com/lti/model/Calc.java
@@ -2,23 +2,8 @@ package com.lti.model;
 
 public class Calc {
 public void add(int num1,int num2) {
-	System.out.print("Sum="+(num1+num2));
+	System.out.print("Sum="+(num1-num2));
 }	
-public void subs(int num1,int num2) {
-	System.out.print("Difference="+(num1-num2));
-}
 
-public void multi(int num1,int num2) {
-	System.out.print("Multiply="+(num1*num2));
-}
-public void div(int num1,int num2){
-System.out.print("Division="+ (num1/num2));
-
-}
-
-public void prod(int num1,int num2) {
-	System.out.print("Multiply="+(num1*num2));
-}
-	
 
 }
